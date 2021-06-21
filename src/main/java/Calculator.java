804fbfdb7ae1839bec4114643c47c41908c6315b
Julian Oliveira <julian.Olliveira@hotmail.com>
2021-06-21 16:33:23 -0300

    - included dependencies junit-jupiter - Class, calculator, created and developed in package java

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+    public int sum(String expression){
+        int sum = 0;
+        for(String valueSum : expression.split("\\+"))
+            sum += Integer.valueOf(valueSum);
+
+        System.out.println(sum);
+        return sum;
+    }
+}
